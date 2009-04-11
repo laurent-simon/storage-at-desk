83bb80af97b6ac85e23700fdcfe31ac9b8774fba
tommy.murphy <tommy.murphy@f077f0aa-1192-11de-9cb0-39e535855b64>
2009-04-11 22:34:15 +0000

    bin not needed

--- trunk/storagedesk/bin/tests/IServer.java
+++
@@ -1,7 +0,0 @@
-package tests;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IServer extends Remote {
-	public long write(byte[] bytes, long position) throws RemoteException;
-}
