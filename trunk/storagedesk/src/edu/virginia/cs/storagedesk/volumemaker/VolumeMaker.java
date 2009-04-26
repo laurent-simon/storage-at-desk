640c5156a4503f231c9e7560d63acc7616412782
tommy.murphy <tommy.murphy@f077f0aa-1192-11de-9cb0-39e535855b64>
2009-04-26 19:40:30 +0000

    volume chunk size and disk block size values set. VolumeMaker input variables modified

--- trunk/storagedesk/src/edu/virginia/cs/storagedesk/volumemaker/VolumeMaker.java
+++ trunk/storagedesk/src/edu/virginia/cs/storagedesk/volumemaker/VolumeMaker.java
@@ -6,10 +6,7 @@ import java.rmi.registry.Registry;
 
 import org.apache.log4j.Logger;
 
-import edu.virginia.cs.storagedesk.common.Config;
-import edu.virginia.cs.storagedesk.common.ISCSI;
 import edu.virginia.cs.storagedesk.common.Util;
-import edu.virginia.cs.storagedesk.database.Mapping;
 import edu.virginia.cs.storagedesk.database.Volume;
 import edu.virginia.cs.storagedesk.volumecontroller.IVolumeController;
 
@@ -33,9 +30,17 @@ public class VolumeMaker {
 		// NOTE: numLUNs * numBlocks * blockSize = total disk size
 		long numLUNs = 1;
 		long numBlocks = 200000;
-		long blockSize = 1024;
+		long blockSize = 1024;	// static: 1KB
 		
 		String targetName = "iqn.edu.virginia.cs.storagedesk:disk3";
+		
+		// volume controller access information
+		String ipAddress = "192.168.5.5";
+		int port = 1099;
+		
+		// this value should be a system wide value and probably
+		// hardcoded into Config.java
+		long volumeChunkSize = 536870912;  // static: 512MB
 		
 		// This block is used to parse command line input
 		try {
@@ -49,25 +54,21 @@ public class VolumeMaker {
 				} else if( args[i].equals("-numBlocks") ) {
 					i++;
 					numBlocks = Long.valueOf(args[i]);
-				} else if( args[i].equals("-sizeBlocks") ) {
-					i++;
-					blockSize = Long.valueOf(args[i]);
 				} else if( args[i].equals("-targetName") ) {
 					i++;
 					targetName = args[i];
-				}
+				} else if( args[i].equals("-ip") ) {
+					i++;
+					ipAddress = args[i];
+				}  else if( args[i].equals("-port") ) {
+					i++;
+					port = Integer.parseInt(args[i]);
+				} 
 			}
 		} catch(Exception e) {
 			System.out.println("Invalid input");
+			return;
 		}
-		
-		// this value should be a system wide value and probably
-		// hardcoded into Config.java
-		long volumeChunkSize = 102400000;
-		
-		// volume controller access information
-		String ipAddress = "192.168.5.5";
-		int port = 1099;
 		
 		Volume volume = new Volume(targetName,
 				numCopies,
@@ -76,7 +77,7 @@ public class VolumeMaker {
 				blockSize,
 				volumeChunkSize);
 
-		boolean isNewVolume = true;
+		//boolean isNewVolume = true;
 		try {
 			// Ask the Volume Controller to register the machine
 			Registry controllerRegistry = LocateRegistry.getRegistry(ipAddress, port);
@@ -87,7 +88,8 @@ public class VolumeMaker {
 			// 1. create a version file for the volume
 			// 2. create a journal directory
 			// 3. create a version file for each virtual chunk
-			isNewVolume = volumeController.isNewVolume(volume);
+			// isNewVolume = volumeController.isNewVolume(volume);
+			volumeController.isNewVolume(volume);
 			volume.setId(volumeController.registerVolume(volume));
 			if (volume.getId() > -1) {
 				//logger.info("Volume registration OK and id is " + volume.getId());
@@ -96,13 +98,12 @@ public class VolumeMaker {
 				//logger.info("Volume registration failed");
 				System.out.println("Volume registration failed");
 				return;
-			}	
-			
-			System.out.println("before mappings");
+			}
 			
 			// Assign or retrieve mappings from the Volume Controller
 			try {
 				volume = volumeController.assignMapping(volume);
+				System.out.println("Volume mappings successfully assigned");
 			} catch (RemoteException e) {
 				System.out.println(e.getCause());
 			}
